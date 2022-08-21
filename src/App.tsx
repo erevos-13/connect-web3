257e4e19597504268b9133b313ba6b84158a6d3a
Orfeas Voutsaridis <ov@wappier.com>
2022-08-21 15:25:35 +0300

    update home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useConnectWeb3 } from "./hooks/useConnectWeb3";
+import Web3 from "web3";
 function App() {
+  const { checkIsWalletConnected, connectWithWallet, network, account } =
+    useConnectWeb3();
+
+  useEffect(() => {
+    checkIsWalletConnected();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex  justify-center">
+      <div className="flex flex-col justify-start">
+        <div className="rounded-full hover:rounded-lg border-green-500 bg-slate-600 w-40 flex justify-center items-center">
+          <button style={{ color: "white" }} onClick={connectWithWallet}>
+            Connect
+          </button>
+        </div>
+
+        <div className="flex flex-col justify-center">
+          <p>
+            Network: <span>{network}</span>
+          </p>
+          <p>
+            Account: <span className="blur hover:blur-none	">{account}</span>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/hooks/useConnectWeb3.tsx
@@ -0,0 +1,50 @@
+import { Web3Provider } from "@ethersproject/providers";
+import { useWeb3React } from "@web3-react/core";
+import { InjectedConnector } from "@web3-react/injected-connector";
+import { useEffect, useState } from "react";
+import Web3 from "web3";
+import { CHAINS } from "../utils/chains";
+
+const useConnectWeb3 = () => {
+  const [network, setNetwork] = useState(null);
+  const injectedConnector = new InjectedConnector({
+    supportedChainIds: [1, 3, 4, 5, 42],
+  });
+  const { chainId, account, activate, active, library } =
+    useWeb3React<Web3Provider>();
+  const { ethereum } = window;
+
+  const getNetWork = () => {
+    const network = CHAINS[chainId as number];
+    setNetwork(network?.name);
+  };
+
+  const connectWithWallet = async () => {
+    try {
+      activate(injectedConnector);
+      getNetWork();
+    } catch (error) {
+      console.log({ error });
+    }
+  };
+
+  const checkIsWalletConnected = () => {
+    if (!ethereum) {
+      console.log("you have not metamask");
+      return;
+    }
+    console.log("metamask is install");
+  };
+  useEffect(() => {
+    if (active) {
+      console.log(account);
+    }
+    if (chainId) {
+      getNetWork();
+    }
+  }, [chainId]);
+
+  return { checkIsWalletConnected, connectWithWallet, network, account };
+};
+
+export { useConnectWeb3 };
